26fe0add5c51a0d8022743294e4be031855ab54b
lostsh <0pdlna3@gmail.com>
2021-10-10 18:12:37 +0200

    now, the cow is working !

--- couw.c
+++ couw.c
@@ -1,32 +1,40 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-void showCow(char*);
-char* parseMessage(char**);
+void showCow(int);
+void showMessage(int, char**);
 int length(char*);
+
 
 int main(int argc, char **argv){
   if(argc == 1){
     printf("Usage: %s [message]\n", argv[0]);
     return 1;
   }
-  showCow(parseMessage(argv));
-  printf("Lengh : %d", length(argv[1]));
+
+  showMessage(argc, argv);
+  showCow(5);
   return 0;
 }
 
-char *parseMessage(char *message[]){
-  //char *msg = NULL;
-  //msg = malloc(1*sizeof(char));
-  //for(int i=1;i<size;i++){
-  //  msg = realloc(
-  //}
-  return message[1];
+void showMessage(int size, char *message[]){
+  int len = size-2;//count spaces between arguments except first arg and first space
+  for(int i=1;i<size;i++) len+=length(message[i]);
+
+  printf(" ");
+  for(int i=0;i<len+2;i++) printf("_");
+  printf("\n<");
+  for(int i=1;i<size;i++) printf(" %s", message[i]);
+  printf(" >\n ");
+  for(int i=0;i<len+2;i++) printf("-");
+  printf("\n");
 }
 
-void showCow(char *message){
-  printf("\n%s\n", message);
-  printf("\\   ^__^\n \\  (oo)\\_______\n    (__) \\       )\\/\\\n         ||----w |\n         ||     ||\n");
+void showCow(int shiftSize){
+  char shift[shiftSize];
+  for(int i=0;i<shiftSize;i++)shift[i] = ' ';
+  shift[shiftSize] = '\0';
+  printf("%s\\   ^__^\n%s \\  (oo)\\_______\n%s    (__) \\       )\\/\\\n%s         ||----w |\n%s         ||     ||\n", shift, shift, shift, shift, shift);
 }
 
 int length(char *txt){
