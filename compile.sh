b0ec9de01fccc36b93eca47fffd88293329dd54c
lostsh <yohann.vernhes@proton.me>
2024-05-10 16:54:10 +0200

    cleanupé

--- compile.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if [ -n "$1" ]; then
-    OUT=$1
-else
-    OUT="cowsay"
-fi
-
-gcc -g -std=c99 -Wall -pedantic -fsanitize=address cow.c -o $OUT && echo -e "[+]\tCompilation success." || echo -e "[-]\tCompilation exception."
